e34089c90bb36e756693df79a20fb82f75f98f8c
bquizza <blaine555@gmail.com>
2019-06-19 12:17:56 -0600

    finished

--- Article/Article.js
+++ Article/Article.js
@@ -15,6 +15,11 @@ class Article {
   expandArticle()  {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open')
+    if (this.expandButton.textContent === 'expand') {
+      this.expandButton.textContent = 'close';
+    } else {
+      this.expandButton.textContent = 'expand';
+    }
   }
 }
 
